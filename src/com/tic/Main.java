4afffac7452d87edb3703ed9de9f6ecfdfdb76d0
gbmao <gabriel.machado.oli@gmail.com>
2025-09-17 08:38:20 -0300

    feat: (play/game) - refactor game() put game logic inside, now main only calls game() -feat play() called by each player to mark an option

--- src/com/tic/Main.java
+++ src/com/tic/Main.java
@@ -5,51 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String[][] game = {{" ", " ", " "},
-                {" ", " ", " "},
-                {" ", " ", " "}};
 
-        String[][] menu = {{"1", "2", "3"},
-                {"4", "5", "6"},
-                {"7", "8", "9"}};
+        game();
 
-        boolean flag = true;
-        Scanner scanner = new Scanner(System.in);
-        String player = "X";
-        String player2 = "0";
-
-        menuRules(menu);
-//        scanner.nextLine();
-
-        while (flag) {
-            ticTacToe(game);
-            System.out.println("-".repeat(10));
-
-            System.out.print("Escolha um numero:");
-
-            try {
-                String response = scanner.nextLine();
-                int res = Integer.parseInt(response);
-                if (res < 1 || res > 9) {
-                    System.out.println("Digite um numero valido");
-                    menuRules(menu);
-                    scanner.nextLine();
-                } else {
-                    boolean notMarked = true;
-
-                    ticTacToe(menu, response, game, player);
-
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Digite um numero valido");
-                menuRules(menu);
-                scanner.nextLine();
-            }
-
-        }
     }
 
-    public static void ticTacToe(String[][] menu, String response, String[][] game, String player) {
+    public static boolean ticTacToe(String[][] menu, String response, String[][] game, String player) {
         for (int i = 0; i < menu.length; i++) {
 
             for (int j = 0; j < menu[i].length; j++) {
@@ -58,17 +19,17 @@ public class Main {
                     if (game[i][j].equals(" ")) {
 //                        menu[i][j] = player;
                         game[i][j] = player;
-
+                        return false; // false == exit from while loop
                     } else {
                         System.out.println(response + " Ja esta marcado!");
-
+                        return true;
                     }
 
                 }
             }
 
         }
-
+        return true;
     }
 
 
@@ -102,6 +63,56 @@ public class Main {
         System.out.println("-Digite qualquer tecla para mostrar esse menu novamente");
         System.out.println("-aperte Enter para continuar a jogar");
         System.out.println("-".repeat(30));
+    }
+
+    public static void play(String player, String[][] game, String[][] menu, Scanner scanner) {
+        boolean flag = true;
+        while (flag) {
+            ticTacToe(game);
+            System.out.println("-".repeat(10));
+
+            System.out.print("Escolha um numero:");
+
+            try {
+                String response = scanner.nextLine();
+                int res = Integer.parseInt(response);
+                if (res < 1 || res > 9) {
+                    System.out.println("Digite um numero valido");
+                    menuRules(menu);
+                    scanner.nextLine();
+                } else {
+
+                    flag = ticTacToe(menu, response, game, player);
+
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um numero valido");
+                menuRules(menu);
+                scanner.nextLine();
+            }
+
+        }
+    }
+
+    public static void game() {
+        String[][] game = {{" ", " ", " "},
+                {" ", " ", " "},
+                {" ", " ", " "}};
+
+        String[][] menu = {{"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}};
+
+
+        Scanner scanner = new Scanner(System.in);
+        String player = "X";
+        String player2 = "0";
+        menuRules(menu);
+        boolean noWinners = true;
+        while (noWinners) {
+            play(player, game, menu, scanner);
+            play(player2, game, menu, scanner);
+        }
     }
 
 
